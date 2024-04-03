7c761acc82c889a6f1a3a841440bd520f6a36871
b40791ja <joshua.alliet@student.manchester.ac.uk>
2024-04-03 21:07:58 +0200

    Finished, but also corrected some errors in main file, and implemented assertions in test file

--- client3.py
+++ client3.py
@@ -30,33 +30,35 @@ N = 500
 
 
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
-    stock = quote['stock']
-    bid_price = float(quote['top_bid']['price'])
-    ask_price = float(quote['top_ask']['price'])
-    price = 0.5 * (bid_price + ask_price)
-    return stock, bid_price, ask_price, price
+	""" Produce all the needed values to generate a datapoint """
+	""" ------------- Update this function ------------- """
+	stock = quote['stock']
+	bid_price = float(quote['top_bid']['price'])
+	ask_price = float(quote['top_ask']['price'])
+	price = 0.5 * (bid_price + ask_price)
+	return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return price_a / price_b if price_b else 0
+	""" Get ratio of price_a and price_b """
+	""" ------------- Update this function ------------- """
+	if price_b:
+		return price_a / price_b
+	return
 
 
 # Main
 if __name__ == "__main__":
-    # Query the price once every N seconds.
-    for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+	# Query the price once every N seconds.
+	for _ in iter(range(N)):
+		quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
-        prices = {}
-        for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
-            prices[stock] = price
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+		""" ----------- Update to get the ratio --------------- """
+		prices = {}
+		for quote in quotes:
+			stock, bid_price, ask_price, price = getDataPoint(quote)
+			prices[stock] = price
+			print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(prices[ list(prices.keys())[0] ], 
-                                    prices[ list(prices.keys())[1] ]))
+		print("Ratio %s" % getRatio(prices[ list(prices.keys())[0] ], 
+									prices[ list(prices.keys())[1] ]))
--- client_test.py
+++ client_test.py
@@ -2,24 +2,45 @@ import unittest
 from client3 import getDataPoint
 
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
-
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+	def test_getDataPoint_calculatePrice(self):
+		quotes = [
+			{'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+			{'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+		]
+		""" ------------ Add the assertion below ------------ """
+		for quote in quotes:
+			self.assertTupleEqual(getDataPoint(quote), ( quote["stock"], quote["top_ask"]["price"], quote["top_bid"]["price"], 0.5 * (quote["top_bid"]["price"] + quote["top_ask"]["price"]) ))
 
 
-  """ ------------ Add more unit tests ------------ """
+	def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+		quotes = [
+			{'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+			{'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+		]
+		""" ------------ Add the assertion below ------------ """
+		for quote in quotes:
+			self.assertTupleEqual(getDataPoint(quote), ( quote["stock"], quote["top_ask"]["price"], quote["top_bid"]["price"], 0.5 * (quote["top_bid"]["price"] + quote["top_ask"]["price"]) ))
+
+
+	""" ------------ Add more unit tests ------------ """
+	def test_getDataPoint_calculatePriceBidLessThan_Ask(self):
+		quotes = [
+			{'top_ask': {'price': 145.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+			{'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+		]
+		for quote in quotes:
+			self.assertTupleEqual(getDataPoint(quote), ( quote["stock"], quote["top_ask"]["price"], quote["top_bid"]["price"], 0.5 * (quote["top_bid"]["price"] + quote["top_ask"]["price"]) ))
+
+	def test_getRatio_calculateRatio(self):
+		a, b = 145.2, 256.9
+		self.assertEqual(getRatio(a, b), a / b)
+
+	def test_getRatio_zeroDenominatorReturnsNull(self):
+		a, b = 145.2, 0
+		self.assertIsInstance(getRatio(a, b), None, "Denominator is zero")
+
 
 
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
